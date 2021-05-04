a135b94e16cb53d075288ae3867df51773aa0607
Antoine <antoine.thenevin@gmail.com>
2021-05-04 11:20:40 +0200

    Setting up figthers scene

---
+++ Assets/Fighter Pack Bundle FREE/Code/FighterAnimationDemoFREE.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FighterAnimationDemoFREE : MonoBehaviour {
+	
+	public Animator animator;
+
+	private Transform defaultCamTransform;
+	private Vector3 resetPos;
+	private Quaternion resetRot;
+	private GameObject cam;
+	private GameObject fighter;
+
+	void Start()
+	{
+		cam = GameObject.FindWithTag("MainCamera");
+		defaultCamTransform = cam.transform;
+		resetPos = defaultCamTransform.position;
+		resetRot = defaultCamTransform.rotation;
+		fighter = GameObject.FindWithTag("Player");
+		fighter.transform.position = new Vector3(0,0,0);
+	}
+
+	void OnGUI () 
+	{
+		if (GUI.RepeatButton (new Rect (815, 535, 100, 30), "Reset Scene")) 
+		{
+			defaultCamTransform.position = resetPos;
+			defaultCamTransform.rotation = resetRot;
+			fighter.transform.position = new Vector3(0,0,0);
+			animator.Play("Idle");
+		}
+
+		if (GUI.RepeatButton (new Rect (25, 20, 100, 30), "Walk Forward")) 
+		{
+			animator.SetBool("Walk Forward", true);
+		}
+		else
+		{
+			animator.SetBool("Walk Forward", false);
+		}
+
+		if (GUI.RepeatButton (new Rect (25, 50, 100, 30), "Walk Backward")) 
+		{
+			animator.SetBool("Walk Backward", true);
+		}
+		else
+		{
+			animator.SetBool("Walk Backward", false);
+		}
+
+		if (GUI.Button (new Rect (25, 80, 100, 30), "Punch")) 
+		{
+			animator.SetTrigger("PunchTrigger");
+		}
+
+		if (GUI.Button (new Rect (25, 110, 100, 30), "Block")) 
+		{
+			GameObject  ChildGameObject = fighter.transform.GetChild(0).gameObject;
+			for (int i = 0; i < ChildGameObject.transform.childCount - 1; i++) {
+				GameObject  SubChildGameObject = ChildGameObject.transform.GetChild(i).gameObject;
+			}
+		}
+	}
+}
