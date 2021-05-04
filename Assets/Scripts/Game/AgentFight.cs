316f0fb417962df9211225073e8a795b05c8fc06
Shen <s_lin97@hotmail.com>
2021-05-04 12:28:32 +0200

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/Game/AgentFight.cs
+++ Assets/Scripts/Game/AgentFight.cs
@@ -9,10 +9,47 @@ class AgentFight
     private GameObject gameObject;
     private int hp;
 
+    public Vector3 targetPos;
     public AgentFight(GameObject go, Animator animator)
     {
         this.gameObject = go;
         this.animator = animator;
+        hp = 100;
+    }
+
+    public State Block()
+    {
+        
+        if(newHP != oldHP)
+        {
+            return State.SUCCESS;
+        }
+        return State.FAILURE;
+    }    
+    public State Punch(AgentFight agent)
+    {
+        Transform transform = gameObject.GetComponent<Transform>();
+        targetPos = GameObject.Find("Enemie").GetComponent<Transform>().position;
+
+        float distance;
+        if(transform.position.x > targetPos.x)
+        {
+            distance = transform.position.x - targetPos.x;
+        }
+        else
+        {
+            distance = targetPos.x - transform.position.x;
+        }
+
+        
+        if ((distance) < 2)
+        {
+            //add animation
+            agent.hp -= 10;
+            return State.SUCCESS;
+        }
+
+        return State.FAILURE;
     }
 
     public State Idle()
