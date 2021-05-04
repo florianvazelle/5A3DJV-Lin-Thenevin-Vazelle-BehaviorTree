89a1901b386eae144e63c674d3fd1a7c539b3bcc
florianvazelle <ponythugflorian@gmail.com>
2021-05-04 12:06:35 +0200

    add wall

--- Assets/Scripts/Game/Common/MouseMove2D.cs
+++ Assets/Scripts/Game/Common/MouseMove2D.cs
@@ -1,23 +1,18 @@
 using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Script pour que le GameObject attaché suive les mouvement de la souris
+/// </summary>
 public class MouseMove2D : MonoBehaviour
 {
 
     private Vector3 mousePosition;
     public float moveSpeed = 0.1f;
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 tmp = Vector2.Lerp(new Vector2(transform.position.x, transform.position.z), new Vector2(mousePosition.x, mousePosition.z), moveSpeed);
         transform.position = new Vector3(tmp.x, transform.position.y, tmp.y);
     }
